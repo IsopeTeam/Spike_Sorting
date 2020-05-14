97a7aded17a9e3f5736385b8ac034965cd01abb9
AnnABCD <65352125+AnnABCD@users.noreply.github.com>
2020-05-14 17:39:21 +0200

    Update and rename 01b_Spikes_By_Cluster_V2.py to 01b_Spikes_waveforms_PSTH_bycluster_V3.py

--- 01b_Spikes_waveforms_PSTH_bycluster_V3.py
+++ 01b_Spikes_waveforms_PSTH_bycluster_V3.py
@@ -7,6 +7,7 @@ Created on Fri Apr 12 17:44:09 2019
 This script allows to extract spike times for each cluster from tridesclous catalogue
 in excel sheet
 + figure plot 
++PSTH
 This script should run in a tridesclous environement where TDC is installed 
 Take a look at the DATA INFO section, fill the info, run and let the magic happen. 
 """
@@ -32,17 +33,11 @@ stim_duration = 1
 water_time = 2.55
 water_duration = 0.15
 
-#Lenght of the single episode (in seconds)
-ep_len = 9.1
-
 #Specify the channel group to explore as [#]. Feel free to do them all : [0,1,2,3]
 channel_groups=[0]
 
 #If True : close figure automatically (avoids to overhelm screen when looping and debug)
 closefig = True
-
-#number of segments
-n_seg=5
 
 #The opacity for the waveform rms. 1. = solid, 0. = transparent 
 wf_alpha =0.2
@@ -63,7 +58,7 @@ dataio = tdc.DataIO(path)
 #Number of segments
 n_seg=dataio.nb_segment
 
-#Compute mean episode lenght
+#Compute mean episode lenght (in seconds)
 sampling_period = 1.0/sampling_rate 
 len_trace=0
 for seg_num in range(n_seg):  
@@ -89,14 +84,7 @@ for chan_grp in channel_groups:
     
     #The segment list for the spikes 
     seg_id = cc.all_peaks['segment']       
-
-    #Positive clusters list
-    clust_list = np.unique(cc.clusters['cluster_label'])
-    mask=(clust_list >= 0)
-    pos_clustlist=clust_list[mask]
-    n_clust=len(pos_clustlist)    
-    
-    
+  
     #The cluster label for median waveform, the median waveforms and the median rms
     waveforms_label = cc.clusters['cluster_label']
     waveforms = cc.centroids_median
@@ -105,6 +93,12 @@ for chan_grp in channel_groups:
     #The probe geometry and specs 
     probe_geometry = cc.geometry
     probe_channels = cc.nb_channel
+    
+    #Positive clusters list
+    clust_list = np.unique(waveforms_label)
+    mask=(clust_list >= 0)
+    pos_clustlist=clust_list[mask]
+    n_clust=len(pos_clustlist)    
 
 
     #Figure for waveforms------------------------------------------------------
@@ -146,15 +140,18 @@ for chan_grp in channel_groups:
     #Spike Times extraction per cluster, aligned segments----------------------------------------        
         
     for cluster, idx in zip(pos_clustlist,range(n_clust)):
-        plt.figure(idx+2)
-        plt.title('Cluster{}'.format(cluster))
-        plt.xlabel('Time (s)')
-        plt.ylabel('Segment')        
+        fig, ax =plt.subplots(2, 1, sharex=True)
+        fig.suptitle('CLUSTER{}'.format(idx), fontsize=16)
+        ax[0].set_title('Raster plot')
+        ax[0].set_ylabel('Segment') 
+        ax[1].set_title('PSTH')
+        ax[1].set_ylabel('Firing rate') 
+        ax[1].set_xlabel('Time (s)')       
         ticks = np.arange(0,ep_len,1)
-        plt.xticks(ticks)
+        ax[1].set_xticks(ticks)
         
-        plt.axvspan(water_time, water_time+water_duration,color='lightcoral',alpha=0.4)
-    #   plt.axvspan(stim_time,stim_time+stim_duration,color='skyblue',alpha=0.6)
+        ax[0].axvspan(water_time, water_time+water_duration,color='skyblue',alpha=0.6)
+        ax[0].axvspan(stim_time,stim_time+stim_duration,color='lightcoral',alpha=0.4)  
         
         SPIKES_clust = [] #To store spikes for each cluster, one array per segment
         #cluster_list = [] #To store the cluster for file indexing 
@@ -166,9 +163,7 @@ for chan_grp in channel_groups:
         for seg_num in range(n_seg):  
             #len_seg = dataio.get_segment_length(seg_num)  
             #time_vector = np.arange(0,len_seg,1)*sampling_period
-
-            temp_ = [] #To store spikes from each cluster
-            
+            temp_ = [] #To store spikes from each cluster            
             for i,j in np.ndenumerate(seg_id):
                 if j == seg_num:
                     if clust_id[i]==cluster:
@@ -178,16 +173,20 @@ for chan_grp in channel_groups:
         
             plt.eventplot(np.ravel(temp_), lineoffsets=seg_num, linelengths=0.5, linewidth=0.5, color=clust_color)
        
+        #PSTH
+        nbins=int(ep_len*6)
+        plt.style.use('seaborn')
+        #ax[1].hist(SPIKES_clust, stacked=True, alpha=0.9)
+        ax[1].hist(np.hstack(SPIKES_clust), nbins, alpha=0.9)
 
-    #SAVE THE SPIKE DATA (or not) ---------------------------------------------    
+        #SAVE THE SPIKE DATA (or not) ---------------------------------------------    
         if savedir != None:
             sorted_spikes = pd.DataFrame(SPIKES_clust,index=seg_list)
             sorted_spikes.to_excel('{}/{}_SPIKETIMES_aligned_CLUSTER{}_changrp{}.xlsx'.format(savedir,name,cluster,chan_grp),index_label='Segment')
             plt.savefig('{}/{}_Spike_times_aligned_CLUSTER{}_changrp{}.pdf'.format(savedir,name,cluster,chan_grp))
         else : 
             print ('No savedir specified : nothing will be saved')
-            
-            
+                        
         if closefig==True:
             plt.close()
      
